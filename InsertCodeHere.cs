6e29241b59c2b91adcad09aff0fbaed60fa600e6
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 13:55:07 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            
+            string title = "Parasite";
+            string genre = "Comedy, Crime, Drama";
+            int year = 2019;
+            int runtime = 132;  // In minutes
+
+            Console.WriteLine("Movie title: " + title);
+            Console.WriteLine("Genre: " + genre);
+            Console.WriteLine("Released: " + year);
+            Console.WriteLine("Runtime: " + runtime + " minutes");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
